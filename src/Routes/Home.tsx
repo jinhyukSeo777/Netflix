f83d75d6e49e7887fcc6b017f5725eb00a9e369a
jinhyukSeo777 <tjwlsgur2556@naver.com>
2022-05-27 16:10:56 +0900

    Final

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -8,8 +8,8 @@ import {
   IGetMoviesResult,
 } from "./api";
 import TopLatedMovie from "./MovieFiles/TopLatedMovie";
-import UpComingMovie from "./MovieFiles/UpcomingMovie";
 import NowPlayingMovie from "./MovieFiles/NowPlayingMovie";
+import UpComingMovie from "./MovieFiles/UpcomingMovie";
 import Footer from "../components/Footer";
 import GetBigMovie from "./MovieFiles/GetBigMovie";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
